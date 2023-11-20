0210feabf54bbab18ec437dcb15523243d00a3b0
Talgat <0_talgat_0@mail.ru>
2023-11-21 01:29:46 +0500

    Добавляет сортировку изображений

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ import { renderGallery } from './gallery.js';
 import './form.js';
 import { loadPictures } from './api.js';
 import { showErrorMessage } from './util.js';
+import { initSorting } from './sorting.js';
 
 const bootstrap = async () => {
   try {
     const pictures = await loadPictures();
     renderGallery(pictures);
+    initSorting(pictures);
   } catch (error) {
     showErrorMessage();
   }
